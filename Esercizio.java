ce6f49a0b216df3d0d784b04d12458b0faccfaa2
giopas <giovanni.pascale@marconibari.edu.it>
2025-10-16 20:08:45 +0000

    consefna

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,43 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+LEGGERE LE ISTRUZIONI NEL FILE README.md
 
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
+import java.util.Random;
 
-// Classe principale, con metodo main
+
 class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+public static void main(String args[]){
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
-    }
+Scanner in = new Scanner(System.in);
+Random rand = new Random();
+
+int N, i, num;
+
+do {
+System.out.print("Inserire la dimensione del vettore: ");
+N = Integer.parseInt(in.nextLine());
+if(N <= 0){
+System.out.println("Inserire un valore maggiore di 0!");
 }
+} while (N <= 0);
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+int[] Vp = new int[N];
+int[] Vd = new int[N];
+int Np = 0;
+int Nd = 0;
+
+i = 0;
+while(i < N){
+num = rand.nextInt(N*10) + 1;
+if(num % 2 == 0){
+Vp[Np] = num;
+++Np;
+}else{
+Vd[Nd] = num;
+++Nd;
+}
+++i;
+}
+}
+}
